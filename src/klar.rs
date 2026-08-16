e18d1ed9eab9151c71370287539eb74c07608d04
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-08-16 12:12:58 -0500

    Revert klar.rs

--- src/klar.rs
+++ src/klar.rs
@@ -21,8 +21,7 @@ impl zed::Extension for KlarExtension {
         // `klar lsp`
         Ok(zed::Command {
             command: self.language_server_binary_path(language_server_id, worktree)?,
-            // Probably enable verbose logging so the user can see the logs in the editor
-            args: vec!["lsp".to_string(), "-v".to_string()],
+            args: vec!["lsp".to_string()],
             env: worktree.shell_env(),
         })
     }
